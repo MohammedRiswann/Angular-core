4f6a5fb93c4cf64e023e849b5adb6d114841fb02
MohammedRiswann <riswan14.rm@gmail.com>
2024-01-22 17:54:57 +0530

    messageimplemented file upload

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -19,23 +19,46 @@ export class FormComponent implements OnInit {
   //     console.log(this.data.controls['name'].touched);
   //   }
   // Reactive form
+  //   myForm!: FormGroup; // <-- Correct type declaration
+  //   email: any;
+  //   constructor(private fb: FormBuilder) {}
+  //   ngOnInit(): void {
+  //     // Fix the syntax issue and properly initialize the form
+  //     this.myForm = this.fb.group({
+  //       name: ['', [Validators.required, Validators.minLength(5)]],
+  //       email: ['', [Validators.required, Validators.email]],
+  //     });
+  //   }
+  //   onSubmit() {
+  //     if (this.myForm.valid) {
+  //       console.log('Form submitted:', this.myForm.value);
+  //     }
+  //   }
+  // File upload
 
-  myForm!: FormGroup; // <-- Correct type declaration
-  email: any;
-
-  constructor(private fb: FormBuilder) {}
-
-  ngOnInit(): void {
-    // Fix the syntax issue and properly initialize the form
-    this.myForm = this.fb.group({
-      name: ['', [Validators.required, Validators.minLength(5)]],
-      email: ['', [Validators.required, Validators.email]],
-    });
-  }
-
-  onSubmit() {
-    if (this.myForm.valid) {
-      console.log('Form submitted:', this.myForm.value);
-    }
-  }
-}
+//   upload!: FormGroup;
+//   constructor(private fb: FormBuilder) {}
+//   ngOnInit() {
+//     this.upload = this.fb.group({
+//       File: [''],
+//     });
+//   }
+//   changing(event: any) {
+//     console.log(event.target.files[0]);
+//     const files = event.target.files[0];
+//     if (files) {
+//       const fileName = files.name;
+//       console.log(fileName);
+//       const formData = new FormData();
+//       formData.append('sudais image', files);
+//       console.log(formData);
+//       this.upload.patchValue({
+//         file: files,
+//       });
+//     }
+//     console.log(event);
+//   }
+//   submitted() {
+//     console.log(this.upload);
+//   }
+// }
